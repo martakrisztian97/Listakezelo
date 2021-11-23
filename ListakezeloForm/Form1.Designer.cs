05adddc7007dfd682c5362ec53e3c8034acc7579
Krisztián Márta <martakrisztian97@gmail.com>
2021-11-23 11:42:03 +0100

    Grafikus elemek hozzáadása és tulajdonságaik beállítása.

--- ListakezeloForm/Form1.Designer.cs
+++ ListakezeloForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace ListakezeloForm {
-  partial class Form1 {
+  partial class formLista {
     /// <summary>
     ///  Required designer variable.
     /// </summary>
@@ -24,22 +24,170 @@ namespace ListakezeloForm {
     ///  the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.lblNev = new System.Windows.Forms.Label();
+      this.txtNev = new System.Windows.Forms.TextBox();
+      this.btnHozzaad = new System.Windows.Forms.Button();
+      this.btnBetu = new System.Windows.Forms.Button();
+      this.btnHatter = new System.Windows.Forms.Button();
+      this.btnSorbarendez = new System.Windows.Forms.Button();
+      this.btnMinden = new System.Windows.Forms.Button();
+      this.btnTorol = new System.Windows.Forms.Button();
+      this.lstLista = new System.Windows.Forms.ListBox();
+      this.btnKilepes = new System.Windows.Forms.Button();
+      this.lblDarab = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
-      // Form1
+      // lblNev
+      // 
+      this.lblNev.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.lblNev.Location = new System.Drawing.Point(20, 20);
+      this.lblNev.Name = "lblNev";
+      this.lblNev.Size = new System.Drawing.Size(200, 50);
+      this.lblNev.TabIndex = 0;
+      this.lblNev.Text = "Írja be a felvivendő nevet!";
+      this.lblNev.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // txtNev
+      // 
+      this.txtNev.Location = new System.Drawing.Point(30, 70);
+      this.txtNev.Name = "txtNev";
+      this.txtNev.Size = new System.Drawing.Size(180, 27);
+      this.txtNev.TabIndex = 1;
+      this.txtNev.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // btnHozzaad
+      // 
+      this.btnHozzaad.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnHozzaad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnHozzaad.Location = new System.Drawing.Point(355, 60);
+      this.btnHozzaad.Name = "btnHozzaad";
+      this.btnHozzaad.Size = new System.Drawing.Size(150, 40);
+      this.btnHozzaad.TabIndex = 2;
+      this.btnHozzaad.Text = "Hozzáad";
+      this.btnHozzaad.UseVisualStyleBackColor = true;
+      // 
+      // btnBetu
+      // 
+      this.btnBetu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnBetu.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnBetu.Location = new System.Drawing.Point(355, 300);
+      this.btnBetu.Name = "btnBetu";
+      this.btnBetu.Size = new System.Drawing.Size(150, 40);
+      this.btnBetu.TabIndex = 3;
+      this.btnBetu.Text = "Betűt színez";
+      this.btnBetu.UseVisualStyleBackColor = true;
+      // 
+      // btnHatter
+      // 
+      this.btnHatter.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnHatter.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnHatter.Location = new System.Drawing.Point(355, 240);
+      this.btnHatter.Name = "btnHatter";
+      this.btnHatter.Size = new System.Drawing.Size(150, 40);
+      this.btnHatter.TabIndex = 4;
+      this.btnHatter.Text = "Hátteret színez";
+      this.btnHatter.UseVisualStyleBackColor = true;
+      // 
+      // btnSorbarendez
+      // 
+      this.btnSorbarendez.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnSorbarendez.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnSorbarendez.Location = new System.Drawing.Point(355, 360);
+      this.btnSorbarendez.Name = "btnSorbarendez";
+      this.btnSorbarendez.Size = new System.Drawing.Size(150, 40);
+      this.btnSorbarendez.TabIndex = 5;
+      this.btnSorbarendez.Text = "Sorbarendez";
+      this.btnSorbarendez.UseVisualStyleBackColor = true;
+      // 
+      // btnMinden
+      // 
+      this.btnMinden.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnMinden.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnMinden.Location = new System.Drawing.Point(355, 180);
+      this.btnMinden.Name = "btnMinden";
+      this.btnMinden.Size = new System.Drawing.Size(150, 40);
+      this.btnMinden.TabIndex = 6;
+      this.btnMinden.Text = "Mindent töröl";
+      this.btnMinden.UseVisualStyleBackColor = true;
+      // 
+      // btnTorol
+      // 
+      this.btnTorol.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnTorol.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnTorol.Location = new System.Drawing.Point(355, 120);
+      this.btnTorol.Name = "btnTorol";
+      this.btnTorol.Size = new System.Drawing.Size(150, 40);
+      this.btnTorol.TabIndex = 7;
+      this.btnTorol.Text = "Töröl";
+      this.btnTorol.UseVisualStyleBackColor = true;
+      // 
+      // lstLista
+      // 
+      this.lstLista.FormattingEnabled = true;
+      this.lstLista.ItemHeight = 20;
+      this.lstLista.Location = new System.Drawing.Point(540, 60);
+      this.lstLista.Name = "lstLista";
+      this.lstLista.Size = new System.Drawing.Size(240, 424);
+      this.lstLista.TabIndex = 8;
+      // 
+      // btnKilepes
+      // 
+      this.btnKilepes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.btnKilepes.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnKilepes.Location = new System.Drawing.Point(585, 498);
+      this.btnKilepes.Name = "btnKilepes";
+      this.btnKilepes.Size = new System.Drawing.Size(150, 40);
+      this.btnKilepes.TabIndex = 9;
+      this.btnKilepes.Text = "Kilépés";
+      this.btnKilepes.UseVisualStyleBackColor = true;
+      // 
+      // lblDarab
+      // 
+      this.lblDarab.Location = new System.Drawing.Point(20, 498);
+      this.lblDarab.Name = "lblDarab";
+      this.lblDarab.Size = new System.Drawing.Size(498, 40);
+      this.lblDarab.TabIndex = 10;
+      this.lblDarab.Text = "A listában az elemek száma: 0";
+      this.lblDarab.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // formLista
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(800, 550);
+      this.Controls.Add(this.lblDarab);
+      this.Controls.Add(this.btnKilepes);
+      this.Controls.Add(this.lstLista);
+      this.Controls.Add(this.btnTorol);
+      this.Controls.Add(this.btnMinden);
+      this.Controls.Add(this.btnSorbarendez);
+      this.Controls.Add(this.btnHatter);
+      this.Controls.Add(this.btnBetu);
+      this.Controls.Add(this.btnHozzaad);
+      this.Controls.Add(this.txtNev);
+      this.Controls.Add(this.lblNev);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-      this.Name = "Form1";
+      this.Name = "formLista";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Listakezelő";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Label lblNev;
+    private System.Windows.Forms.TextBox txtNev;
+    private System.Windows.Forms.Button btnHozzaad;
+    private System.Windows.Forms.Button btnBetu;
+    private System.Windows.Forms.Button btnHatter;
+    private System.Windows.Forms.Button btnSorbarendez;
+    private System.Windows.Forms.Button btnMinden;
+    private System.Windows.Forms.Button btnTorol;
+    private System.Windows.Forms.ListBox lstLista;
+    private System.Windows.Forms.Button btnKilepes;
+    private System.Windows.Forms.Label lblDarab;
   }
 }
 
